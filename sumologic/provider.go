2876a87927dbeb2393bbd22399dc4319bcf4bf0a
Vishal Sahu <vishal@test.com>
2020-09-20 11:20:38 -0700

    Add Lookup Table resource

--- sumologic/provider.go
+++ sumologic/provider.go
@@ -64,6 +64,7 @@ func Provider() terraform.ResourceProvider {
 			"sumologic_monitor":                            resourceSumologicMonitorsLibraryMonitor(),
 			"sumologic_monitor_folder":                     resourceSumologicMonitorsLibraryFolder(),
 			"sumologic_ingest_budget_v2":                   resourceSumologicIngestBudgetV2(),
+			"sumologic_lookup_table":                       resourceSumologicLookupTable(),
 		},
 		DataSourcesMap: map[string]*schema.Resource{
 			"sumologic_caller_identity": dataSourceSumologicCallerIdentity(),
